fc6b27b1de3cfeded324f19cfed75b2a89253e65
namuan <575441+namuan@users.noreply.github.com>
2023-02-25 21:30:54 +0000

    refactor: Fix audio script to use Pytube

--- save-mp3.py
+++ save-mp3.py
@@ -1,40 +1,52 @@
 #!python3
 
 from __future__ import unicode_literals
-import yt_dlp as youtube_dl
-import appex
-import console
-import clipboard
+
 import os
-import sys
-from pathlib import Path
-import shutil
+
+import appex
+import clipboard
+import console
+from pytube import YouTube
 
 outdir = os.path.expanduser("~/Documents/Downloads/mp3")
 try:
-	os.mkdir(outdir)
+    os.mkdir(outdir)
 except FileExistsError:
-	pass
-	
+    pass
+
+url = None
+
 if appex.get_attachments():
-	# e.g. share from YouTube app
-	url = appex.get_attachments()[0]
+    # e.g. share from YouTube app
+    url = appex.get_attachments()[0]
 elif appex.get_urls():
-	# e.g. share from Safari
-	url = appex.get_urls()[0]
+    # e.g. share from Safari
+    url = appex.get_urls()[0]
 elif appex.get_text():
-	url = appex.get_text()
+    url = appex.get_text()
 elif clipboard.get():
-	url = clipboard.get()
-	
+    url = clipboard.get()
+
 print("URL: ", url)
 if not url or not url.startswith("http"):
-	url = input("No URL found - enter URL to download: ")
-	
-ydl_opts = {'outtmpl': os.path.join(outdir, '%(title)s.%(ext)s')}
-ydl_opts['format'] = 'bestaudio'
-	
-with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-		info = ydl.extract_info(url, download=True)
-		filepath = ydl.prepare_filename(info)
-		console.open_in(filepath)
+    url = input("No URL found - enter URL to download: ")
+
+
+def download_audio(url):
+    yt = YouTube(url)
+    print("Starting download of", yt.title)
+    console.show_activity()
+    downloaded_file = yt.streams \
+        .filter(only_audio=True, mime_type="audio/mp4") \
+        .order_by('abr') \
+        .desc() \
+        .first() \
+        .download(output_path=outdir)
+    print("Downloaded to", downloaded_file)
+    console.hide_activity()
+    return downloaded_file
+
+
+downloaded_file = download_audio(url)
+console.open_in(downloaded_file)
