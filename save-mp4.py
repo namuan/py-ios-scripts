e057e1574dd4178ff1ab513ca2f4bee7f13ebfe8
namuan <575441+namuan@users.noreply.github.com>
2023-02-25 21:09:55 +0000

    refactor: Using pytube instead of yt-dlp

--- save-mp4.py
+++ save-mp4.py
@@ -1,45 +1,42 @@
 #!python3
 
 from __future__ import unicode_literals
-import yt_dlp as youtube_dl
 import appex
 import console
 import clipboard
 import os
 import sys
+from pytube import YouTube
 
 outdir = os.path.expanduser("~/Documents/Downloads")
 try:
-	os.mkdir(outdir)
+    os.mkdir(outdir)
 except FileExistsError:
-	pass
-	
+    pass
+
+url = None
 if appex.get_attachments():
-	# e.g. share from YouTube app
-	url = appex.get_attachments()[0]
+    # e.g. share from YouTube app
+    url = appex.get_attachments()[0]
 elif appex.get_urls():
-	# e.g. share from Safari
-	url = appex.get_urls()[0]
+    # e.g. share from Safari
+    url = appex.get_urls()[0]
 elif appex.get_text():
-	url = appex.get_text()
+    url = appex.get_text()
 elif clipboard.get():
-	url = clipboard.get()
-	
+    url = clipboard.get()
+
 print("URL: ", url)
 if not url or not url.startswith("http"):
-	url = input("No URL found - enter URL to download: ")
-	
-ydl_opts = {
-	'outtmpl': os.path.join(outdir, '%(title)s.%(ext)s'),
-	"socket_timeout": 10,
-	"retries": 10,
-	"noplaylist": True,
-	"quiet": True,
-	"no_warnings": True,
-}
+    url = input("No URL found - enter URL to download: ")
 
-with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-	info = ydl.extract_info(url, download=True)
-	filepath = ydl.prepare_filename(info)
-
-console.open_in(filepath)
+yt = YouTube(url)
+print("Starting download of", yt.title)
+downloaded_file = yt.streams\
+    .filter(progressive=True, file_extension='mp4')\
+    .order_by('resolution')\
+    .desc()\
+    .first()\
+    .download(output_path=outdir)
+print("Downloaded to", downloaded_file)
+# console.open_in(downloaded_file)
